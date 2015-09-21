5df25b00ccfe215b010fb58813216cfd286dc7a8
unknown <TRREI@LT-819018.statoil.net>
2015-09-21 12:35:44 +0200

    new version

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = express();
 var port = (process.env.PORT || 3000);
 console.log("Server running. Listening on port " + port);
 app.get('/', function(req, res) {
-	res.send('Hello ' + req.query.name);
+	res.send('Hello ' + (req.query.name || 'John Doe'));
 });
 app.listen(port);
