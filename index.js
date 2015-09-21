0fe8e54b8d7c920452ee2d4e639ab06ec19e5834
unknown <TRREI@LT-819018.statoil.net>
2015-09-21 13:35:34 +0200

    new version

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var port = (process.env.PORT || 3000);
-console.log("Server running. Listening on port " + port);
-app.get('/', function(req, res) {
-	res.send('Hello ' + (req.query.name || 'John Doe'));
-});
-app.listen(port);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TRREIExpress' });
+});
+
+router.get('/store', function(req, res, next) {
+  //res.render('index', { title: 'TRREIExpress' });
+  res.write('Test');
+  res.end();
 });
 
 module.exports = router;
