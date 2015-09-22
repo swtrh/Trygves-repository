4356e592604b53908cb2634b728fb510ce42c549
unknown <TRREI@LT-819018.statoil.net>
2015-09-22 09:15:37 +0200

    A working version

--- routes/store.js
+++ routes/store.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var mongoURL = 'mongodb://trrei:trrei@ds051523.mongolab.com:51523/message_db';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('store', { title: 'Express Store' });
 });
 
+/*
 router.route('/message')
   .post(function(req, res,next) {
       console.log('Receiving message ' + req.body.message);
       res.send('Message was ' + req.body.message);
   });
+*/
+
+
+ router.route('/message').post(function (req, res, next) {
+    var txtMessage = (req.body.message || 'empty message');
+    console.log('Receiving message ' + txtMessage);
+ // Storing message in database
+    MongoClient.connect(mongoURL, function(err, db) {
+        console.log("Connected to database");
+
+        db.collection('messages').insert({'message': txtMessage}, {w: 1 }, function (err, item) {
+            if (err) {
+                console.log('Error storing message in database: ' + err);
+                db.close();
+                res.status(400).send('Error, unable to store message: ' + txtMessage);
+            } else {
+                db.close();
+                console.log('Message stored ok in database: ' + txtMessage)
+                res.status(200).send('Message stored: "' + txtMessage + '"');
+            }
+        });
+    });
+    // End storing message in database
+ });
 
 module.exports = router;
